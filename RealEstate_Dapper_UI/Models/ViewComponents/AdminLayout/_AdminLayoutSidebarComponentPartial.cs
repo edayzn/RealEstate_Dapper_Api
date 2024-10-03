7f6e73aecb8da05be44ce7b40be1a568024d9a83
edayzn <eda.yzn5@gmail.com>
2024-10-03 23:38:21 +0300

    03.10

---
+++ RealEstate_Dapper_UI/Models/ViewComponents/AdminLayout/_AdminLayoutSidebarComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Models.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutSidebarComponentPartial:ViewComponent
+
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
