1a9661e972edbce9f3955cf4198f698f49d2b0e0
edayzn <eda.yzn5@gmail.com>
2024-10-11 10:53:14 +0300

    10.10

---
+++ RealEstate_Dapper_Api/Repositories/StatisticsRepositories/IStatisticsRepository.cs
@@ -0,0 +1,37 @@
+﻿using RealEstate_Dapper_Api.Dtos.EmployeeDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.StatisticsRepositories
+{
+    public interface IStatisticsRepository
+    {
+        int CategoryCount();
+        int ActiveCategoryCount();
+        int PassiveCategoryCount();
+        int ProductCount();
+        //Toplam Daire Sayısı
+        int ApartmenCount();
+        string EmployeeNameByMaxProductCount();
+        string CategoryNameByMaxProductCount();
+        //Ortala Fiyat
+        decimal AverageProductPriceByRent();
+        decimal AverageProductPriceBySale();
+
+      //  ilan içerisinde en fazla hangi şehir var
+        string CityNameByMaxProductCount();
+
+        //Kaç farklı şehirden ilan var
+        int DifferentCityCount();
+
+        //En son yüklenen ilan fiyatı
+        decimal LastProductPrice();
+
+        //En Yeni ve En eski Bina
+        string NewestBuildingYear();
+        string OldestBuildingYear();
+
+        //ortalama oda sayısı
+        int AverageRoomCount();
+
+        int ActiveEmployeeCount();
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/StatisticsRepositories/StatisticsRepository.cs
@@ -0,0 +1,179 @@
+﻿
+using Dapper;
+using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Dtos.EmployeeDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.StatisticsRepositories
+{
+    public class StatisticsRepository : IStatisticsRepository
+    {
+        private readonly Context _context;
+
+        public StatisticsRepository(Context context)
+        {
+            _context = context;
+        }
+        public int  ActiveCategoryCount()
+        {
+            string query = " Select Count(*) From Category where CategoryStatus=1";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values =  conncetion.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public int ActiveEmployeeCount()
+        {
+            string query = " Select Count(*) From Employee where Status=1";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public int ApartmenCount()
+        {
+            string query = "Select Count(*) From Product where Title Like '%Daire%'";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<int>(query);
+                return values;
+            }
+        }
+
+        public decimal AverageProductPriceByRent()
+        {
+            string query = "Select AVG(Price) From Product where Type='Kiralık'";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<decimal>(query);
+                return values;
+            };
+        }
+
+        public decimal AverageProductPriceBySale()
+        {
+            string query = "Select AVG(Price) From Product where Type='Satılık'";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<decimal>(query);
+                return values;
+            };
+        }
+
+        public int AverageRoomCount()
+        {
+            string query = "Select Avg(RoomCount) From ProductDetails";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<int>(query);
+                return values;
+            };
+        }
+
+        public int CategoryCount()
+        {
+            string query = "Select Count(*) From Category";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<int>(query);
+                return values;
+            };
+        }
+
+        public string CategoryNameByMaxProductCount()
+        {
+            string query = "Select top(1) CategoryName,Count(*) From Product inner join Category On Product.ProductCategory=Category.CategoryID Group By CategoryName order By Count(*) Desc";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<string>(query);
+                return values;
+            };
+        }
+
+        public string CityNameByMaxProductCount()
+        {
+            string query = "Select top(1) City,Count(*) From Product Group By City order By Count(*) Desc";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<string>(query);
+                return values;
+            };
+        }
+
+        public int DifferentCityCount()
+        {
+            string query = "Select Count(Distinct( City)) From Product ";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<int>(query);
+                return values;
+            };
+        }
+
+        public string EmployeeNameByMaxProductCount()
+        {
+            string query = "Select Name,Count(*) 'product_count' From Product inner join Employee On Product.EmployeeID=Employee.EmployeeID Group By Name Order By product_count Desc";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<string>(query);
+                return values;
+            };
+        }
+
+        public decimal LastProductPrice()
+        {
+            string query = "Select top(1) Price From Product Order By ProductID Desc";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<decimal>(query);
+                return values;
+            };
+        }
+
+        public string NewestBuildingYear()
+        {
+            string query = "Select top(1) BuildYear From ProductDetails Order By BuildYear Desc";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<string>(query);
+                return values;
+            };
+        }
+
+        public string OldestBuildingYear()
+        {
+            string query = "Select top(1) BuildYear From ProductDetails Order By BuildYear";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<string>(query);
+                return values;
+            };
+        }
+
+        public int PassiveCategoryCount()
+        {
+            
+            string query = "Select Count(*) From Category Where CategoryStatus=0";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<int>(query);
+                return values;
+            };
+        }
+
+        public int ProductCount()
+        {
+
+            string query = "Select Count(*) From Product";
+            using (var conncetion = _context.CreateConnection())
+            {
+                var values = conncetion.QueryFirstOrDefault<int>(query);
+                return values;
+            };
+        }
+    }
+}
