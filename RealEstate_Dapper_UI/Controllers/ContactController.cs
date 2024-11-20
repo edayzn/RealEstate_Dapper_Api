ddad6f3b376444a883d075fb5d9403b154100912
edayzn <eda.yzn5@gmail.com>
2024-11-20 23:26:32 +0300

    SignalR

---
+++ RealEstate_Dapper_UI/Controllers/ContactController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class ContactController : Controller
+    {
+       
+    }
+}
---
+++ RealEstate_Dapper_UI/Models/ViewComponents/Dashboard/_DashboardLastContactListComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ContactDtos;
+
+namespace RealEstate_Dapper_UI.Models.ViewComponents.Dashboard
+{
+    public class _DashboardLastContactListComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardLastContactListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44305/api/Contacts/GetLastFourContact");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<LastFourContactResultDto>>(jsonData);
+                return View(values);
+            }
+            return View(); ;
+        }
+    }
+}
